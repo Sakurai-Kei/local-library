b57e9b3db26ebdbf7a3af8e22a677f1f6b42c881
Sakurai Kei <luqmanulraiserhakim@gmail.com>
2022-03-24 16:59:18 +0800

    Added files and changes for deployment to Heroku

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var compression = require('compression');
+var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,7 +13,8 @@ var catalogRouter = require('./routes/catalog');
 
 var app = express();
 
-var mongoDB = require('./mongodb-connection-string').mongourl();
+var dev_db_url = 'mongodb+srv://m001-student:m001-mongodb-basics@locallibrary.ttxnq.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, "MongoDB connection error:"));
@@ -20,10 +23,12 @@ db.on('error', console.error.bind(console, "MongoDB connection error:"));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
